240983a28e4fa8be23cbb09533977390051df8c4
Eric Marchand <phimage@users.noreply.github.com>
2019-11-02 07:42:39 +0100

    Create Dangerfile.swift

---
+++ Dangerfile.swift
@@ -0,0 +1,23 @@
+import Danger
+import DangerXCodeSummary // package: https://github.com/f-meloni/danger-swift-xcodesummary.git
+import Foundation
+
+
+let danger = Danger()
+
+let github = danger.github
+
+
+// Make it more obvious that a PR is a work in progress and shouldn't be merged yet
+if danger.github.pullRequest.title.contains("WIP") {
+    warn("PR is classed as Work in Progress")
+}
+
+// Warn when there is a big PR
+if (danger.github.pullRequest.additions ?? 0) > 500 {
+    warn("Big PR, try to keep changes smaller if you can")
+}
+let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
+
+// SwiftLint
+SwiftLint.lint(inline: true, configFile: ".swiftlint.yml")
